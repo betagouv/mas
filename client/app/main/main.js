9dfe627b6e032e81f5237e5143fe69bf6f1eebc9
Thibaut Cantet <thibaut.cantet@gmail.com>
2016-01-25 15:17:48 +0100

    init solution

---
+++ client/app/main/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('masApp')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
